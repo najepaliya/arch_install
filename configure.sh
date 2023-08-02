7bc412f6dc5c0c0d7a0b103e8af9ca117b342348
Neel Jepaliya <najepaliya@gmail.com>
2023-08-02 08:31:39 -0700

    add more context to configuration variables

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 presets=(base gnome plasma)
-base="base linux linux-firmware power-profiles-daemon sudo" # essential
-extras="amd-ucode fakeroot fish" # non-essential
+base="base linux linux-firmware power-profiles-daemon sudo" # essential shared
+extras="amd-ucode fakeroot fish" # non-essential, potentially unique
 gnome="$base gdm gnome-bluetooth-3.0 gnome-console network-manager-applet $extras" # essential for gnome
 plasma="$base bluedevil konsole kscreen kwallet-pam plasma-desktop plasma-nm plasma-pa plasma-wayland-session powerdevil sddm-kcm $extras" # essential for plasma
 commands=(
