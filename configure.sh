069b3f31fd927200858838c4eaa8f670367d65bd
najepaliya <133005733+najepaliya@users.noreply.github.com>
2024-02-22 16:27:26 -0700

    Update gnome preset

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 presets=(base gnome plasma)
 base="base linux linux-firmware power-profiles-daemon sudo" # essential shared
 extras="amd-ucode fakeroot fish" # non-essential, potentially unique
-gnome="$base gdm gnome-bluetooth-3.0 gnome-console network-manager-applet $extras" # essential for gnome
+gnome="$base gdm gnome-console gnome-control-center networkmanager xdg-desktop-portal-gnome $extras" # essential for gnome
 plasma="$base bluedevil konsole kscreen kwallet-pam plasma-desktop plasma-nm plasma-pa plasma-wayland-session powerdevil sddm-kcm $extras" # essential for plasma
 
 
