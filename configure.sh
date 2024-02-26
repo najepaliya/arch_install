a5b555531e52edd06e5972dcc5188ec70732aad9
najepaliya <133005733+najepaliya@users.noreply.github.com>
2024-02-25 21:59:12 -0700

    Simplify KDE Plasma dependencies

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@ presets=(base gnome plasma)
 base="base linux linux-firmware power-profiles-daemon sudo" # essential shared
 extras="amd-ucode fakeroot fish" # non-essential, potentially unique
 gnome="$base gdm gnome-console gnome-control-center networkmanager xdg-desktop-portal-gnome $extras" # essential for gnome
-plasma="$base bluedevil konsole kscreen kwallet-pam plasma-desktop plasma-nm plasma-pa plasma-wayland-session powerdevil sddm-kcm $extras" # essential for plasma
+plasma="$base konsole kwallet-pam plasma-desktop plasma-nm plasma-pa plasma-wayland-session powerdevil sddm-kcm $extras" # essential for plasma
 
 
 locale="en_US.UTF-8 UTF-8" # used for locale-gen and locale.conf
